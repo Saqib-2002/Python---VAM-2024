89e6b60f87d0f325ade520c349efb6a3a94246d3
Saqib Saifi <saqibsaifi720@gmail.com>
2024-03-05 12:30:23 +0530

    5 March

--- Github/3_march.py
+++ Github/3_march.py
@@ -1,6 +1,7 @@
 # 1- WAP to find the count of repeated character from user input.
 
-char = input("Enter any String.\n")
+a = input("Enter any String.\n")
+
 
 # 2- WAP to find the prime number 
 # 3- WAP to find the repeated character from user input.
---
+++ March 2024/5_march.py
@@ -0,0 +1,68 @@
+''' Internship domains
+Python
+    Data Analytics -> Solve traffic problem
+    Full Stack Web Dev (MERN)
+    Digital Marketing
+    Domains - Wanifashion.com
+            - Affiliate Marketing Web -> ridewithAnjali.com
+            - spinball.co.in -> mobileApp 
+            - aws
+'''
+
+''' Statistics mathematics -> mean - sum of values/ count of values
+                              median - mid value
+                              mode - highest repeat value
+                              standard deviation - sq E(x-u)pow2 / N where, N = count, x = dataSet value, sd = standard deviation
+                              variance - (sd)pow2
+                              percentile - p/100 * N
+                              .
+    Discrete mathematics'''
+import numpy as np
+import statistics as stats
+import matplotlib.pyplot as plt
+
+x = [1, 3, 7, 5, 9, 3, 11]
+
+# mean
+print(np.mean(x))
+
+# median
+print(np.median(x))
+
+# mode
+print(stats.mode(x))
+
+# Standard Deviation
+print(np.std(x))
+
+# Variance
+print(np.var(x))
+
+# Percentile
+print(np.percentile(x, 9))
+
+# Normal Distribution - np.random.normal( , , )
+# Uniform Distribution - np.random.uniform( , , )
+
+a = np.random.normal(0, 10, 100)
+b = np.random.uniform(0, 10, 100)
+
+plt.bar(a, b)
+# plt.bar()
+plt.show()
+
+data = ["Pawan", "Rohan", "Akshay", "Tripti"]
+
+# Ques 1 - You have a dataSet data, do Sorting in ascending order
+# Ques 2 - replace A with Z and T with X and N with W
+# Ques 3 - 1/0 using exception handling
+# Ques 4 - Take Student Name from user and store it in student class and than access it in department class.
+
+
+''' Try to be in US, UK Startups. Don't choose indian startup.
+    US Startup - BOLD, EPMA -> 45LPA, Walmart, Deloitte
+    Checkout for the opportunities in Career option
+    Japenense Startup - Evolution.vc.com, Cainz, anotheraddress.jp
+'''
+
+# mySQl - DBA - company profile -> Database Administrator
